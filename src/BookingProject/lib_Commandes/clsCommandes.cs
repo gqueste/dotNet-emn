c0d939ac62e33b602bf1c27e1db029895fa20f0d
gqueste <gabriel.queste@gmail.com>
2015-05-07 16:43:26 +0200

    Changement fonctionnement lib_Commandes

--- src/BookingProject/lib_Commandes/clsCommandes.cs
+++ src/BookingProject/lib_Commandes/clsCommandes.cs
@@ -16,19 +16,41 @@ namespace lib_Commandes
         [AutoComplete]
         public void reservation(int idVol, int idHotel, DateTime date, String nomUtilisateur)
         {
+            reserveVol(idVol, date, nomUtilisateur);
+            reserveHotel(idHotel, date, nomUtilisateur);
+        }
+
+        private void reserveVol(int idVol, DateTime date, String nomUtilisateur)
+        {
             SqlConnection MyConnection = new SqlConnection();
             MyConnection.ConnectionString = "Data Source=FR-92-02-14-008;Initial Catalog=BANK_FR;Integrated Security=True";
             MyConnection.Open();
-            SqlCommand MyCommand = new SqlCommand("sp_reservation", MyConnection);
+            SqlCommand MyCommand = new SqlCommand("sp_reservation_vol", MyConnection);
             MyCommand.CommandType = CommandType.StoredProcedure;
             MyCommand.Parameters.Add("@ID_VOL", SqlDbType.VarChar);
             MyCommand.Parameters["@ID_VOL"].Value = idVol;
+            MyCommand.Parameters.Add("@DATE", SqlDbType.DateTime);
+            MyCommand.Parameters["@DATE"].Value = date;
+            MyCommand.Parameters.Add("@NOM_UTILISATEUR", SqlDbType.VarChar);
+            MyCommand.Parameters["@NOM_UTILISATEUR"].Value = nomUtilisateur;
+
+            MyCommand.ExecuteScalar();
+            MyConnection.Close();
+        }
+
+        private void reserveHotel(int idHotel, DateTime date, String nomUtilisateur)
+        {
+            SqlConnection MyConnection = new SqlConnection();
+            MyConnection.ConnectionString = "Data Source=FR-92-02-14-008;Initial Catalog=BANK_FR;Integrated Security=True";
+            MyConnection.Open();
+            SqlCommand MyCommand = new SqlCommand("sp_reservation_hotel", MyConnection);
+            MyCommand.CommandType = CommandType.StoredProcedure;
             MyCommand.Parameters.Add("@ID_HOTEL", SqlDbType.VarChar);
             MyCommand.Parameters["@ID_HOTEL"].Value = idHotel;
             MyCommand.Parameters.Add("@DATE", SqlDbType.DateTime);
             MyCommand.Parameters["@DATE"].Value = date;
             MyCommand.Parameters.Add("@NOM_UTILISATEUR", SqlDbType.VarChar);
-            MyCommand.Parameters["@NOM_UTILISATEUR"].Value = idHotel;
+            MyCommand.Parameters["@NOM_UTILISATEUR"].Value = nomUtilisateur;
 
             MyCommand.ExecuteScalar();
             MyConnection.Close();
