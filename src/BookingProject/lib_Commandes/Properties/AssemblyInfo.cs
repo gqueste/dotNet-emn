0315e620079d3d031adf8df0598809069118eb3b
gqueste <gabriel.queste@gmail.com>
2015-05-08 14:31:57 +0200

    Rectifications SQL

--- src/BookingProject/lib_Commandes/Properties/AssemblyInfo.cs
+++ src/BookingProject/lib_Commandes/Properties/AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.EnterpriseServices;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 [assembly: ApplicationActivation(ActivationOption.Server)]
 [assembly: ApplicationName("FlightBooking")]
 
